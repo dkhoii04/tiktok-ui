5391b217ba3fc939a519acf121f1501a884b1b98
DucKhoi <doduckhoi04@gmail.com>
2025-12-18 20:57:22 +0700

    Tái cấu trúc code

---
+++ src/layouts/HeaderOnly/index.js
@@ -0,0 +1,14 @@
+import Header from '~/layouts/components/Header'; //do trùng Header nên import riêng
+
+function HeaderOnly({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="layout-body">
+                <div className="layout-content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderOnly;
